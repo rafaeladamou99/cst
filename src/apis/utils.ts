0fd8df271df6d9ba26676185053fe79b431947da
rafaeladamou99 <rafaeladamou99@gmail.com>
2023-10-01 09:54:21 +0300

    Test

--- src/apis/utils.ts
+++ src/apis/utils.ts
@@ -1,10 +1,7 @@
 export const useHandleApiError = (error: {
   response: { status: number; data: { message: string } }
 }) => {
-  // const statusCode = error?.response?.status
-  // if (statusCode === 409 || statusCode === 401) {
   const errorMsg = error?.response?.data?.message
 
   return errorMsg
-  // }
 }
